8e725dbd3e60e759f1b1210dc21599e2b01b9850
Christof Franke <kraut@wurzt.de>
2010-10-27 16:07:12 +0200

    simplified

--- tagging_autocomplete/views.py
+++ tagging_autocomplete/views.py
@@ -3,19 +3,31 @@ from tagging.models import Tag
 from django.http import HttpResponse
 from django.utils import simplejson
 
-
 def list_tags(request):
-	try:
-	    tags = Tag.objects.filter(name__istartswith=request.GET['term']).values_list('name', flat=True)
-	except MultiValueDictKeyError:
-		tags = []
-	
-	return JsonResponse([x.encode('utf-8') for x in tags])
+    print "autocompl"
+    try:
+        tags = Tag.objects.filter(name__istartswith=request.GET['q']).values_list('name', flat=True)
+    except MultiValueDictKeyError:
+        pass
 
+    return HttpResponse('\n'.join(tags), mimetype='text/plain')
 
-class JsonResponse(HttpResponse):
-    """
-    HttpResponse descendant, which return response with ``application/json`` mimetype.
-    """
-    def __init__(self, data):
-        super(JsonResponse, self).__init__(content=simplejson.dumps(data), mimetype='application/json')
+#def list_tags(request):
+#	print "autocomp"
+#	print request.GET['q']
+#
+#	try:
+#	    tags = Tag.objects.filter(name__istartswith=request.GET['q']).values_list('name', flat=True)
+#
+#	except MultiValueDictKeyError:
+#		tags = []
+#	
+#	return JsonResponse([x.encode('utf-8') for x in tags])
+#
+#
+#class JsonResponse(HttpResponse):
+#    """
+#    HttpResponse descendant, which return response with ``application/json`` mimetype.
+#    """
+#    def __init__(self, data):
+#        super(JsonResponse, self).__init__(content=simplejson.dumps(data), mimetype='application/json')
--- tagging_autocomplete/widgets.py
+++ tagging_autocomplete/widgets.py
@@ -4,17 +4,25 @@ from django.conf import settings
 from django.utils.safestring import mark_safe
 
 class TagAutocomplete(TextInput):
+    
+    def render(self, name, value, attrs=None):
+        list_view = reverse('tagging_autocomplete-list')
+        if not 'class' in attrs:
+            attrs.update({'class' : 'vTextField'})
+        html = super(TagAutocomplete, self).render(name, value, attrs)
+        js = u'''<script type="text/javascript"> 
+        jQuery().ready(function() {  
+            jQuery("#%s").autocomplete("%s",{ 
+            multiple: true, 
+            autoFill: true, 
+            multipleSeparator: ",", 
+            scroll: true });
+            });
+        </script>''' 	% (attrs['id'], list_view)
+        return mark_safe("\n".join([html, js]))
 	
-	def render(self, name, value, attrs=None):
-		list_view = reverse('tagging_autocomplete-list')
-		if not 'class' in attrs:
-			attrs.update({'class' : 'vTextField'})
-		html = super(TagAutocomplete, self).render(name, value, attrs)
-		js = u'<script type="text/javascript">$(function() {installTaggingAutocompleate("%s", "%s") });</script>' % (attrs['id'], list_view)
-		return mark_safe("\n".join([html, js]))
-	
-	class Media:
-		js_base_url = getattr(settings, 'TAGGING_AUTOCOMPLETE_JS_BASE_URL','%s/jquery-autocomplete' % settings.MEDIA_URL)
-		js = (
-			'%s/tagging_autocomplete.js' % js_base_url,
-		)
+	#class Media:
+	#	js_base_url = getattr(settings, 'TAGGING_AUTOCOMPLETE_JS_BASE_URL','%s/jquery-autocomplete' % settings.MEDIA_URL)
+	#	js = (
+	#		'%s/tagging_autocomplete.js' % js_base_url,
+	#	)
